682016ff49b1c4980d39a06260bd3f600e8d8605
khaliloualdchaib <Khalil.OualdChaib@student.uantwerpen.be>
2021-11-16 06:44:30 -0800

    installed sfml + made window in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
-using namespace std;
-int main(){
-cout<<"hello world";
-return 0;
+#include <SFML/Graphics.hpp>
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear(sf::Color::Black);
+    }
+
+    return 0;
 }

9eda14298465102f03f601fcad89419506875471
khaliloualdchaib <Khalil.OualdChaib@student.uantwerpen.be>
2021-12-09 11:22:44 -0800

    abstract factory

---
+++ src/logic_library/include/abstractFactory.h
@@ -0,0 +1,25 @@
+//
+// Created by khalil on 12/9/21.
+//
+
+#ifndef DOODLEJUMP_ABSTRACTFACTORY_H
+#define DOODLEJUMP_ABSTRACTFACTORY_H
+namespace DoodleJump{
+    class AbstractFactory{
+    public:
+
+        /*
+         * This is a default constructor.
+         */
+        AbstractFactory() = default;
+
+        /*
+         * This is a default destructor
+         */
+        virtual ~AbstractFactory() = default;
+
+        //The following functions create and return the entities:
+        
+    };
+}
+#endif //DOODLEJUMP_ABSTRACTFACTORY_H
